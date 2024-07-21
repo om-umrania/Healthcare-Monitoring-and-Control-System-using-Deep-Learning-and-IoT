5971beaa84c6f3f25eae29c71b57fc16a96207e1
om-umrania-code <60146744+om-umrania@users.noreply.github.com>
2024-07-21 17:54:13 +0530

    Major Improvements in Healthcare Monitoring

--- notebooks/Chapter 5/CryptographicFunctions.py
+++ notebooks/Chapter 5/CryptographicFunctions.py
@@ -1,28 +1,27 @@
-def findHash(block) :
-    import hashlib
-    block_data = str(block['src']) + str(block['dest']) + str(block['data']) + str(block['ts']) + str(block['nonce']) + str(block['prevHash'])
-    
-    return hashlib.sha256(str(block_data).encode('utf-8')).hexdigest()
+import hashlib
 
-def checkForUniqueHash(blockchain, hashVal) :
-    if(len(blockchain) > 1) :
+def findHash(block):
+    block_data = str(block['src']) + str(block['dest']) + str(block['data']) + str(block['ts']) + str(block['nonce']) + str(block['prevHash'])
+    return hashlib.sha256(block_data.encode('utf-8')).hexdigest()
+
+def checkForUniqueHash(blockchain, hashVal):
+    if len(blockchain) > 1:
         conditions = findCondition(blockchain[0]['nonce'])
         l = len(conditions)
-        if(hashVal[0:l] != conditions) :
+        if hashVal[0:l] != conditions:
             return False
     
-    for count in range(1, len(blockchain)) :
-        if(blockchain[count]['hash'] == hashVal) :
+    for count in range(1, len(blockchain)):
+        if blockchain[count]['hash'] == hashVal:
             return False
     
     return True 
 
-def findCondition(nonce) :
+def findCondition(nonce):
     l = len(str(nonce))
-    l = int(round((20 - l)/8))
+    l = int(round((20 - l) / 8))
     val = ""
-    for count in range(0, l) :
-        val = val + "0"
+    for count in range(l):
+        val += "0"
     
     return val
-    
--- notebooks/Chapter 5/Node.py
+++ notebooks/Chapter 5/Node.py
@@ -1,100 +1,88 @@
+import math
+import random
+import CryptographicFunctions as cf
+
 class Node:
-    def __init__(self, name, location, trust = 0, energy = 1000):
+    def __init__(self, name, location, trust=0, energy=1000):
         self.name = name
         self.trust = trust
         self.energy = energy
         self.location = location
         self.blockchain = []
-        
-    def modifyTrust(n, newTrust) :
-        n.trust = n.trust + newTrust
-        if(n.trust <= 0) :
-            n.trust = 1
-            
+
+    def modifyTrust(self, newTrust):
+        self.trust += newTrust
+        if self.trust <= 0:
+            self.trust = 1
         return True
-    
-    def canAddBlock(n, block) :
-        import time
-        import random
-        import CryptographicFunctions as cf
-        
+
+    def canAddBlock(self, block):
         hash_val = cf.findHash(block)
-        isUnique = cf.checkForUniqueHash(n.blockchain, hash_val)
+        isUnique = cf.checkForUniqueHash(self.blockchain, hash_val)
         return isUnique
-    
-    def getTrustScore(n1, n2, check = True) :
-        import math
-        
-        loc1 = n1.location
-        loc2 = n2.location
-        
-        x1 = loc1[0]
-        y1 = loc1[1]
-        x2 = loc2[0]
-        y2 = loc2[1]
-        
-        dist = math.sqrt((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2))
-        trust_val = ((n2.energy)/(dist+1)) + (n2.trust)/(n1.trust+1)
-        if(n1.validateBlockchain() and n2.validateBlockchain()) :
+
+    def getTrustScore(self, other_node, check=True):
+        loc1 = self.location
+        loc2 = other_node.location
+
+        dist = math.sqrt((loc1[0] - loc2[0]) ** 2 + (loc1[1] - loc2[1]) ** 2)
+        trust_val = (other_node.energy / (dist + 1)) + (other_node.trust / (self.trust + 1))
+
+        if self.validateBlockchain() and other_node.validateBlockchain():
             return trust_val
-        elif(check == True) :
+        elif check:
             print('\tSelf correcting chains...')
-            n1.correctBlockchain()
-            n2.correctBlockchain()
+            self.correctBlockchain()
+            other_node.correctBlockchain()
             return trust_val
-        else :
+        else:
             return trust_val
-    
-    def storeBlockchain(n, blockchain) :
-        n.blockchain = blockchain
+
+    def storeBlockchain(self, blockchain):
+        self.blockchain = blockchain
         return True
-    
-    def addBlock(n, block) :
-        #print('Node %s, blocks %d' % (n.name, len(n.blockchain)))
-        n.blockchain.append(block)
+
+    def addBlock(self, block):
+        self.blockchain.append(block)
         return True
-    
-    def attackNode(n, threshold) :
-        import random
+
+    def attackNode(self, threshold):
         val = random.random()
-        if(val < threshold) :
-            index = round(random.random() * (len(n.blockchain)-1))
-            if(index < 0) :
+        if val < threshold:
+            index = round(random.random() * (len(self.blockchain) - 1))
+            if index < 0:
                 return False
-            try :
-                n.blockchain[index]['prevHash'] = ''
-                n.reduceEnergy()
-                print('\tAttacking node %s' % (n.name))
-            except :
+            try:
+                self.blockchain[index]['prevHash'] = ''
+                self.reduceEnergy()
+                print(f'\tAttacking node {self.name}')
+            except:
                 print('\tWill attack in next iteration')
-                
             return True
-        else :
+        else:
             return False
-        
-    def correctBlockchain(n) :
-        for count in range(2, len(n.blockchain)) :
-            
-            prevHash = n.blockchain[count-1]['hash']
-            n.blockchain[count]['prevHash'] = prevHash
-        
-        n.reduceEnergy()
-        print('\tChain corrected for node %s...' % (n.name))
+
+    def correctBlockchain(self):
+        for count in range(2, len(self.blockchain)):
+            prevHash = self.blockchain[count - 1]['hash']
+            self.blockchain[count]['prevHash'] = prevHash
+
+        self.reduceEnergy()
+        print(f'\tChain corrected for node {self.name}...')
         return True
-    
-    def reduceEnergy(n) :
-        if(n.energy > 0) :
-            n.energy = n.energy - 1
+
+    def reduceEnergy(self):
+        if self.energy > 0:
+            self.energy -= 1
         return True
-    
-    def validateBlockchain(n) :
-        for count in range(2, len(n.blockchain)) :
-            
-            prevHash = n.blockchain[count-1]['hash']
-            if(n.blockchain[count]['prevHash'] != prevHash) :
-                n.modifyTrust(-1)
-                return False;
-        
-        n.reduceEnergy()
-        n.modifyTrust(1)
+
+    def validateBlockchain(self):
+        for count in range(2, len(self.blockchain)):
+            prevHash = self.blockchain[count - 1]['hash']
+            if self.blockchain[count]['prevHash'] != prevHash:
+                self.modifyTrust(-1)
+                return False
+
+        self.reduceEnergy()
+        self.modifyTrust(1)
         return True
