abb089f6be99ade26635a6a0aa1782fc06c7f160
om-umrania-code <60146744+om-umrania@users.noreply.github.com>
2024-07-22 10:49:54 +0530

    Update Enhanced_PoS_Consensus.py

--- notebooks/Chapter 5/Enhanced_PoS_Consensus.py
+++ notebooks/Chapter 5/Enhanced_PoS_Consensus.py
@@ -3,15 +3,13 @@ import pandas as pd
 import random
 import time
 import CryptographicFunctions as cf
-import sys
 import heartpy as hp
 import matplotlib.pyplot as plt
+import Node
 
 def run():
     data_vals, _ = hp.load_exampledata(0)
-    # plt.plot(data_vals)
-    # plt.show()
-
+    
     numBlocks = 1000
     maxNodes = 50
     nodeStakes = [1] * maxNodes
@@ -23,65 +21,61 @@ def run():
     ts1 = time.time()
     counter2 = 0
 
-    for count in range(numBlocks):
-        src = round(random.random() * (maxNodes - 1))
+    for count in range(0, numBlocks):
+        src = round(random.random() * (maxNodes-1))
         nodeStake = nodeStakes[src]
         dest = round(random.random() * maxNodes)
-        data = round(random.random() * (maxData - 1))
+        data = round(random.random() * (maxData-1))
         data = data_vals[data]
         ts = time.time()
-        
+
         if count == 0:
             prevHash = ''
         else:
-            prevHash = blockchain[counter2 - 1]['hash']
-        
-        block = {
-            'src': src,
-            'dest': dest,
-            'data': data,
-            'ts': ts,
-            'prevHash': prevHash,
-            'nonce': nodeStake + round(random.random() * src) + round(random.random() * ts)
-        }
-        
+            prevHash = blockchain[counter2-1]['hash']
+
+        block = {}
+        block['src'] = src
+        block['dest'] = dest
+        block['data'] = data
+        block['ts'] = ts
+        block['prevHash'] = prevHash
+
+        # Apply Pos+PoW (use stake of src node & random timestamp for mining)
+        block['nonce'] = nodeStake + round(random.random() * src) + round(random.random() * ts)
+
         hash_val = cf.findHash(block)
         isUnique = cf.checkForUniqueHash(blockchain, hash_val)
         counter = 0
-        while not isUnique:
+        while isUnique == False:
             block['nonce'] = nodeStake + round(random.random() * src) + counter + round(random.random() * ts)
-            counter += 1
+            counter = counter + 1
             hash_val = cf.findHash(block)
             isUnique = cf.checkForUniqueHash(blockchain, hash_val)
-        
-        nodeStakes[src] += 1
-        
+
+        nodeStakes[src] = nodeStake + 1
+
         ts2 = time.time()
-        delay = ts2 - ts
-        avgDelay += delay
-        
+        delay = (ts2 - ts)
+        avgDelay = avgDelay + delay
+
         block['hash'] = hash_val
-        
+
         if count % 2 == 0:
             blockchain.append(block)
-            counter2 += 1
-        
-        print(f'Processed block {count}, Delay {delay:.06f} s')
-        
-    if len(blockchain) > 0:
-        avgDelay = avgDelay / len(blockchain)
-        print(f'Ensemble Average delay to mine the block: {avgDelay:.04f} s')
-    else:
-        print('No blocks were added to the blockchain.')
+            counter2 = counter2 + 1
 
+        print('Processed block %d, Delay %0.06f s' % (count, delay))
+
+    avgDelay = avgDelay / len(blockchain) if len(blockchain) > 0 else 0
+
+    print('Ensemble Average delay to mine the block: %0.04f s' % (avgDelay))
     ts2 = time.time()
-    print(f'Ensemble Total delay: {ts2 - ts1:.04f} s')
+    print('Ensemble Total delay: %0.04f s' % (ts2-ts1))
 
-    results = {
-        "avg_delay": avgDelay,
-        "total_delay": ts2-ts1,
-        "blockchain": blockchain
+    # Return results for visualization
+    return {
+        'avg_delay': avgDelay,
+        'total_delay': ts2 - ts1,
+        'blockchain': blockchain
     }
-
-if __name__ == "__main__":
-    run()
